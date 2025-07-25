79b904cb11d8b5b34cdfc97158dccb7aad83ddcf
sharo-jef <sharo.jef@gmail.com>
2025-07-26 00:45:07 +0900

    add: cargo ci alias

---
+++ ci/src/main.rs
@@ -0,0 +1,27 @@
+use std::process::Command;
+
+fn main() {
+    let tasks = vec![
+        ("fmt", vec!["cargo", "fmt"]),
+        ("check", vec!["cargo", "check"]),
+        ("test", vec!["cargo", "test"]),
+        ("clippy", vec!["cargo", "clippy"]),
+        ("build", vec!["cargo", "build", "--release"]),
+        (
+            "install cargo-audit",
+            vec!["cargo", "install", "cargo-audit", "--root", "target/tools"],
+        ),
+        ("audit", vec!["./target/tools/bin/cargo-audit", "audit"]),
+    ];
+
+    for (name, command) in tasks {
+        println!("> {}", command.join(" "));
+        let status = Command::new(&command[0])
+            .args(&command[1..])
+            .status()
+            .expect("Failed to execute command");
+        if !status.success() {
+            panic!("Command `{}` failed with status: {}", name, status);
+        }
+    }
+}
